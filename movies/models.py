b0ac34c289b151761cec5ecf2d68bb070d1a2711
Rodya <rodyavasimov@yandex.ru>
2021-10-24 20:46:05 +0300

    Added templates

--- movies/models.py
+++ movies/models.py
@@ -75,8 +75,8 @@ class Movie(models.Model):
     def __str__(self):
         return self.title
 
-    # def get_absolute_url(self):
-    #     return reverse("movie_detail", kwargs={"slug": self.url})
+    def get_absolute_url(self):
+        return reverse("movie_detail", kwargs={"slug": self.url})
 
     class Meta:
         verbose_name = "Фильм"
--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.views import View
+from django.views.generic import ListView, DetailView
 
-# Create your views here.
+from .models import Movie
+
+
+class MovieView(ListView):
+    model = Movie
+    queryset = Movie.objects.filter(draft=False)
+    template_name = 'movies/movie_list.html'
+
+
+class MovieDetailView(DetailView):
+    model = Movie
+    slug_field = 'url'
+    template_name = 'movies/movie_detail.html'

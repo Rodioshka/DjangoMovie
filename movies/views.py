95a0d3d49d7feca6d9cde6ee4665a2d03d38ca3e
Rodya <rodyavasimov@yandex.ru>
2021-10-25 23:32:44 +0300

    Addet filter

--- movies/views.py
+++ movies/views.py
@@ -1,12 +1,23 @@
+from django.db.models import Q
 from django.shortcuts import render, redirect
 from django.views import View
 from django.views.generic import ListView, DetailView
 
-from .models import Movie, Category
+from .models import Movie, Category, Actor, Genre
 from .forms import ReviewForm
 
 
-class MovieView(ListView):
+class GenreYear:
+    """Жанры и года фильмов"""
+
+    def get_genres(self):
+        return Genre.objects.all()
+
+    def get_years(self):
+        return Movie.objects.filter(draft=False).values("year")
+
+
+class MovieView(GenreYear, ListView):
     model = Movie
     queryset = Movie.objects.filter(draft=False)
     template_name = 'movies/movie_list.html'
@@ -17,7 +28,7 @@ class MovieView(ListView):
     #     return context
 
 
-class MovieDetailView(DetailView):
+class MovieDetailView(GenreYear, DetailView):
     model = Movie
     slug_field = 'url'
     template_name = 'movies/movie_detail.html'
@@ -35,3 +46,21 @@ class AddReview(View):
             form.movie = movie
             form.save()
         return redirect(movie.get_absolute_url())
+
+
+class ActorView(GenreYear, DetailView):
+    """Вывод информации об актере"""
+    model = Actor
+    template_name = 'movies/actor.html'
+    slug_field = 'name'
+
+
+class FilterMoviesView(GenreYear, ListView):
+    """Фильтр фильмов"""
+
+    def get_queryset(self):
+        queryset = Movie.objects.filter(
+            Q(year__in=self.request.GET.getlist("year")) |
+            Q(genres__in=self.request.GET.getlist("genre"))
+        )
+        return queryset
